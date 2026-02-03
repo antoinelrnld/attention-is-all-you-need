c41e7ba7367419e85ded06e9c1083801a301fcce
antoinelrnld <38052726+antoinelrnld@users.noreply.github.com>
2026-02-03 23:58:22 +0100

    solution: positional_encoding

--- src/positional_encoding.py
+++ src/positional_encoding.py
@@ -1,10 +1,28 @@
+import torch
 import torch.nn as nn
+import math
 
 
 class PositionalEncoding(nn.Module):
     def __init__(self, d_model: int, seq_len: int, dropout: float):
         super().__init__()
-        pass
+        self.d_model = d_model
+        self.seq_len = seq_len
+        self.dropout = nn.Dropout(dropout)
+
+        # Create a matrix of shape (seq_len, d_model)
+        pos_enc = torch.zeros(seq_len, d_model)
+        # Create a vector of shape (seq_len, 1)
+        position = torch.arange(0, seq_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        # Apply the sin to even position
+        pos_enc[:, 0::2] = torch.sin(position * div_term)
+        pos_enc[:, 1::2] = torch.cos(position * div_term)
+
+        pos_enc = pos_enc.unsqueeze(0) # (1, seq_len, d_model)
+
+        self.register_buffer('pos_enc', pos_enc) # Save the tensor in the model file
     
     def forward(self, x):
-        pass
+        x = x + (self.pos_enc[:, :x.shape[1], :]).requires_grad(False)
+        return self.dropout(x)
