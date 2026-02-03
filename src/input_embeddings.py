a78c18e70a561634506f8f652de4da056c858050
antoinelrnld <38052726+antoinelrnld@users.noreply.github.com>
2026-02-03 23:09:44 +0100

    skeleton: input_embeddings

---
+++ src/input_embeddings.py
@@ -0,0 +1,10 @@
+import torch.nn as nn
+
+
+class InputEmbeddings(nn.Module):
+    def __init__(self, d_model: int, vocab_size: int):
+        super().__init__()
+        pass
+    
+    def forward(self, x):
+        pass
