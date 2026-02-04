e9e1a08419b9189ebed6fdcc9037e519e85fbcfa
antoinelrnld <38052726+antoinelrnld@users.noreply.github.com>
2026-02-04 23:18:48 +0100

    solution: layer_normalization

--- src/layer_normalization.py
+++ src/layer_normalization.py
@@ -3,9 +3,13 @@ import torch.nn as nn
 
 
 class LayerNormalization(nn.Module):
-    def __init__(self, eps: float = 1e-6):
+    def __init__(self, features: int, eps: float = 1e-6):
         super().__init__()
-        pass
+        self.eps = eps
+        self.alpha = nn.Parameter(torch.ones(features)) # Multiplied
+        self.bias = nn.Parameter(torch.zeros(features)) # Added
 
     def forward(self, x):
-        pass
+        mean = x.mean(dim=-1, keepdim=True)
+        std = x.std(dim=-1, keepdim=True)
+        return self.alpha * (x - mean) / (std + self.eps) + self.bias
