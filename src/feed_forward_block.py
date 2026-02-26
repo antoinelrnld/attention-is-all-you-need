fa7523e0514f2b100333ae2f9255864513a6a74e
antoinelrnld <38052726+antoinelrnld@users.noreply.github.com>
2026-02-26 23:04:11 +0100

    solution: feed_forward

--- src/feed_forward_block.py
+++ src/feed_forward_block.py
@@ -5,7 +5,10 @@ from torch import nn
 class FeedForwardBlock(nn.Module):
     def __init__(self, d_model: int, d_ff: int, dropout: float) -> None:
         super().__init__()
-        pass
+        self.linear_1 = nn.Linear(d_model, d_ff)
+        self.dropout = nn.Dropout(dropout)
+        self.linear_2 = nn.Linear(d_ff, d_model)
     
     def forward(self, x):
-        pass
+        # (Batch, Seq_Len, d_model) --> (Batch, Seq_Len, d_ff) --> (Batch, Seq_Len, d_model)
+        return self.linear_2(self.dropout(torch.relu(self.linear_1(x))))
